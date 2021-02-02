a1cf1629a003997f4cbbea392e73e0a6e49c0e72
TienDzung <dzungnt2412@gmail.com>
2021-02-02 10:50:25 +0700

    a

--- core/constants/audio.go
+++ core/constants/audio.go
@@ -4,5 +4,5 @@ package constants
 const (
 	AUDIO_PATH    = "../../public/audio"
 	VIETTEL_API   = "https://viettelgroup.ai/voice/api/tts/v1/rest/syn"
-	TOKEN_VIETTEL = "NvFFJLCkQUqCGDIqZGK4MWDA1dQuEzEqzbltdgyEjdR1a65GPUA6-zqc46TYNp9b"
+	TOKEN_VIETTEL = "ZKoay1HlxiW43pf3-UksscycpbR6IZB6X1X-qbSz1FMaRX-EUCswEUv7uFmk-7pP"
 )
